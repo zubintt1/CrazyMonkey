67fc3c424f4d12f61eeb0a4f2f4a06334fe95f5d
= <=>
2017-05-26 18:45:33 +0530

    Office Eod Day One

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 #! /bin/env python
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class StageOneOperator(BoxLayout):
+	# """docstring for StageOneOperator"""
+	# def __init__(self, arg):
+	# 	super(StageOneOperator, self).__init__()
+	# 	self.arg = arg
+		
+	pass
 
 
 class FileOperator(App):
@@ -9,4 +19,4 @@ class FileOperator(App):
 
 if __name__ == '__main__':
 		FileOperator().run()
-		
+			
