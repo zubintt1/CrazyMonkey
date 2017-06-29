e4a7323d31f076912810a305683a9d8a2b64f749
Bodhiswattwa Tewari <zubin_zorro@rediff.com>
2017-06-29 17:10:11 +0530

    FTP Login Check Completed

--- main.py
+++ main.py
@@ -18,7 +18,19 @@ class Master_Control(BoxLayout):
     alpha = 0
     server_port = 0
 
-    def upload_file(self):
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            file_content = stream.read()
+
+        return file_content
+        # self.dismiss_popup()
+
+    def upload_file(self, path, filename):
+        # print("Path="+str(path)+"\nFile Name="+str(filename))
+        content = self.load(path,filename)
+        self._popup = Popup(title="Load file", content=content,size_hint=(0.9, 0.9))
+        self._popup.open()
         self.alpha = self.alpha + 1
         print(self.alpha)
 
@@ -30,13 +42,14 @@ class Master_Control(BoxLayout):
         host = '127.0.0.1'
         port = self.server_port
 
-        ftp_connection = FTP(user='user',passwd='12345')
+        ftp_connection = FTP()
         try:
             ftp_connection.connect(host,port)
-            ftp_connection.login()
+            message = ftp_connection.login('user','12345')
+            print(message)
         except:
             print(sys.exc_info()[0])
-        print(sys.exc_info()[0])
+        print("Login Check Done")
 
     def start_ftp_server(self):
         a = Ftp_Control.random_port_gen(self)
@@ -53,21 +66,16 @@ class Master_Control(BoxLayout):
     #     self._popup = Popup(title="Load file", content=content,size_hint=(0.9, 0.9))
     #     self._popup.open()
 
-#     def load(self, path, filename):
-#         with open(os.path.join(path, filename[0])) as stream:
-#             self.text_input.text = stream.read()
-#         self.dismiss_popup()
-#
-#     def dismiss_popup(self):
-#         self._popup.dismiss()
+
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
 #
 #     def work_on_something(self):
 #         print("Hello Zubin")
 #
 #
-# class LoadDialog(FloatLayout):
-#     load = ObjectProperty(None)
-#     cancel = ObjectProperty(None)
+
 
 class FileOperator(App):
     pass
